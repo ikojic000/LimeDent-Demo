add85c125cae0e27c2f313b3458e747faaa069c7
Ivan Kojic <ikojic000@gmail.com>
2023-03-23 11:09:37 +0100

    ...

--- LimeDent Demo/src/com/ikojic/demo/Start.java
+++ LimeDent Demo/src/com/ikojic/demo/Start.java
@@ -20,7 +20,7 @@ public class Start {
 		Patient patient = new Patient( 1 , "Test Test" );
 		
 		ImageCompressionThread compressionThread = new ImageCompressionThread(
-				new File( "C:/Users/ikojic000/Pictures/Zagreb_Advent_2022/Finished/_MG_2161.jpg" ) , patient );
+				new File( "../../../Images/01 Login.png" ) , patient );
 		
 		compressionThread.start();
 		
